870fa1a0fac857ab20dff79be8647839bb5c8ab5
unknown <Zurong.Luo@YATSENGLOBAL.COM>
2020-11-24 16:05:25 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- <Button>222</Button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{msg}}11
+    <HelloWorld/>
+  </div>
+</template>
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      msg: '首页'
+    }
+  }
+}
+</script>
